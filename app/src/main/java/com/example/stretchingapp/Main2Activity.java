9978103ee76d49a6a67dcb0060f84212a40cc9cd
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-02-05 15:01:49 -0600

    Adding a scrolling method
    Didn't work :-1:

--- app/src/main/java/com/example/stretchingapp/Main2Activity.java
+++ app/src/main/java/com/example/stretchingapp/Main2Activity.java
@@ -2,12 +2,35 @@ package com.example.stretchingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button goBack;
+    private TextView Scrolling;
+    public List UserCreatedList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Scrolling = findViewById(R.id.ListView);
+        goBack = findViewById(R.id.goBack);
+        Scrolling.setMovementMethod(new ScrollingMovementMethod());
+
+
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
     }
 }
