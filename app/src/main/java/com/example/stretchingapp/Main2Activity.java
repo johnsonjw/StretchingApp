1dfe4db23d55e8bd1eeee5f04059e4acd0d455c3
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-02-05 20:47:16 -0600

    Adding Popup Window for User Input
    Made it so that a list was created whenever the user presses Create List it Creates a new unamed list

--- app/src/main/java/com/example/stretchingapp/Main2Activity.java
+++ app/src/main/java/com/example/stretchingapp/Main2Activity.java
@@ -7,11 +7,13 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
 
     private Button goBack;
+    private Button ListCreate;
     private TextView Scrolling;
     public List UserCreatedList;
 
@@ -20,11 +22,8 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Scrolling = findViewById(R.id.ListView);
+        ListCreate = findViewById(R.id.CreateList);
         goBack = findViewById(R.id.goBack);
-        Scrolling.setMovementMethod(new ScrollingMovementMethod());
-
-
-
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,5 +31,17 @@ public class Main2Activity extends AppCompatActivity {
             }
 
         });
+
+        ListCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List newExcerciseList = new ArrayList();
+                System.out.println("What would you like to name the list");
+                String p = "";
+
+            }
+        });
+
+        }
     }
 }
