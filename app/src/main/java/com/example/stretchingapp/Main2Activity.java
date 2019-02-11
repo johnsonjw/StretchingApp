94308b4aae104b387afbd51dd4191e250b31c6ef
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-02-10 18:41:08 -0600

    Added a Spinner List of Exercises

--- app/src/main/java/com/example/stretchingapp/Main2Activity.java
+++ app/src/main/java/com/example/stretchingapp/Main2Activity.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.*;
 
 public class Main2Activity extends AppCompatActivity  {
 
@@ -23,9 +21,13 @@ public class Main2Activity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Spinner theSpinster = findViewById(R.id.ListOfStretches);
         //Scrolling = findViewById(R.id.ListView);
         ListCreate = findViewById(R.id.UserCreateList);
         goBack = findViewById(R.id.goBack);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Main2Activity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.StretchesNames));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        theSpinster.setAdapter(myAdapter);
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
